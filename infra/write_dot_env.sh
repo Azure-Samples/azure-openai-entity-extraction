43fa890393e5da05cbdd5a610d8590ef49ba4747
Pamela Fox <pamela.fox@gmail.com>
2025-04-10 09:20:47 -0700

    Write out OPENAI_HOST

--- infra/write_dot_env.sh
+++ infra/write_dot_env.sh
@@ -4,6 +4,7 @@
 > .env
 
 # Append new values to the .env file
+echo "OPENAI_HOST=azure" >> .env
 echo "AZURE_OPENAI_GPT_DEPLOYMENT=$(azd env get-value AZURE_OPENAI_GPT_DEPLOYMENT)" >> .env
 echo "AZURE_OPENAI_SERVICE=$(azd env get-value AZURE_OPENAI_SERVICE)" >> .env
 echo "AZURE_TENANT_ID=$(azd env get-value AZURE_TENANT_ID)" >> .env
